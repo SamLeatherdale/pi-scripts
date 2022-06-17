68b923dafa29d4fa518ba7358438dddd66f696dd
Sam Leatherdale <SamLeatherdale@users.noreply.github.com>
2022-06-17 21:48:22 +1000

    Update aliases basepath

--- shell/init.fish
+++ shell/init.fish
@@ -1,5 +1,4 @@
 # Add this to your real ~/.config/fish/config.fish:
 #   source $HOME/pi-scripts/shell/init.fish
-set CURDIR "$HOME/pi-scripts/shell"
 
-source "$CURDIR/aliases.sh"
+source "$HOME/pi-scripts/shell/aliases.sh"
