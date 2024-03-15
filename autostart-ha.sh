c6db2e40d464c1236fc2702987139c3b1c54ecee
Sam Leatherdale <SamLeatherdale@users.noreply.github.com>
2024-03-15 22:26:00 +1100

    Switch to dev server command

--- autostart-ha.sh
+++ autostart-ha.sh
@@ -3,4 +3,4 @@ sleep 10 # Wait for HA to start
 export BROWSER_ARGS="--profile-directory=Default --start-fullscreen"
 export BROWSER="/usr/bin/chromium-browser"
 cd /home/pi/homeboard
-npx vite preview --open
+npm run start -- --open
