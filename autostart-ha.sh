983bf458b8a5e8d3995a0e31cf19f9c96cc704dd
Sam Leatherdale <SamLeatherdale@users.noreply.github.com>
2025-10-05 11:52:37 +1100

    Update HomeBoard autostart

--- autostart-ha.sh
+++ autostart-ha.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env zsh
 sleep 10 # Wait for HA to start
-export BROWSER_ARGS="--profile-directory=Default --start-fullscreen"
-export BROWSER="/usr/bin/chromium-browser"
-cd /home/pi/homeboard
-npm run start -- --open
+# Clear crashed flag
+# https://superuser.com/questions/1343290/disable-chrome-session-restore-popup
+sed -i 's/\"exit_type\":\"Crashed\"/\"exit_type\":\"Normal\"/g' \
+    "/home/pi/.config/chromium/Default/Preferences"
+# Open browser
+/usr/bin/chromium-browser --profile-directory=Default --start-fullscreen --password-store=basic https://homeboard.samleatherdale.com
